9a712a15942db4f828803718ad2360796046b6f0
Ali Shiraee <a.shiraee@gmail.com>
2025-08-18 16:01:17 -0400

    docs: include documentations and training configs and artifacts

--- src/contrastors/trainers/__init__.py
+++ src/contrastors/trainers/__init__.py
@@ -1,6 +1,5 @@
 from .base import *
 from .glue import *
-from .image_text import *
 from .mlm import *
 from .text_text import *
 
@@ -8,6 +7,4 @@ TRAINER_REGISTRY = {
     "mlm": MLMTrainer,
     "glue": GlueTrainer,
     "encoder": TextTextTrainer,
-    "clip": ImageTextTrainer,
-    "locked_text": ImageTextTrainer,
 }
